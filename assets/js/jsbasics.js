db7cec19032f9dcf10ea2c20f15b1774b399e812
peter waweru-eng <wawerupeterwamui@gmail.com>
2024-10-16 13:27:16 +0300

    initial commit

---
+++ assets/js/jsbasics.js
@@ -0,0 +1,9 @@
+const=colorbutton=document.getElementById('colorbutton');
+
+colorbutton.addEventListener('click',()=>{
+    colorbutton.style.backgroundColor= colorbutton.style.backgroundColor === 'blue'?'white':'blue';
+}
+);
+
+
+
